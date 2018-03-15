a78cd8ab50a59119989b8b6c50d7c95457d17dde
Simon Zheng <583621555@qq.com>
2018-03-15 15:31:30 +0800

    modify settings

--- PetHospital/PetHospital/settings.py
+++ PetHospital/PetHospital/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'basic_structure',
+    'role_learning',
+    'case_study',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'roleLearning.apps.RolelearningConfig',
-    'caseStudy.apps.CasestudyConfig',
 ]
 
 MIDDLEWARE = [
