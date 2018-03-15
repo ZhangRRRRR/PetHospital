517257af99f08d4b4feb4851be16cea7d90c51cc
jasonWang <353213847@qq.com>
2018-03-15 13:08:35 +0800

    delete the cosplay

--- PetHospital/PetHospital/settings.py
+++ PetHospital/PetHospital/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'roleLearning.apps.RolelearningConfig'
 ]
 
 MIDDLEWARE = [
